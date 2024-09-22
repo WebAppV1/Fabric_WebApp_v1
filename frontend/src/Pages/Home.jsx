e7ea80a91b3a813e289483f2feb822c0b6ba9c06
VEGAD MITESH M. <miteshvegad148@gmail.com>
2024-09-22 10:05:04 -0400

    Creating UI navbar

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      {/* Navbaris successfully created */}
+    </div>
+  )
+}
+
+export default Home
